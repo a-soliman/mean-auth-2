72a5820a8f78b160c7ff9b937d0bd6cf330c62da
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-01-10 08:59:50 -0800

    intalled packages

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 const express		= require('express');
 const bodyParser 	= require('body-parser');
 const path 			= require('path');
+const logger 		= require('morgan');
+const cookieParser 	= require('cookie-parser');
+const session		= require('express-session');
+const passport 		= require('passport');
+const localStrategy = require('passport-local').Strategy;
+const multer		= require('multer');
+const flash 		= require('connect-flash');
+const mongo 		= require('mongodb');
+const mongoose 		= require('mongoose');
 
-
-const app = express();
-const port = process.env.PORT || 3000;
+// Initializing the db using mongoose
+const db = mongoose.connection;
 
 // API for interacting with MongoDB
 const api = require('./server/routes/api');
 
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Logger
+app.use(logger('dev'));
+
 // Parsers
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 //Angular dist output folder
 app.use(express.static(path.join(__dirname, 'dist')));
