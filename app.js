155bf25d10000d9fca5dbef7528022e053a2533c
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-01-14 15:07:56 -0800

    preparing for heroku deploy

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ const db = mongoose.connection;
 const api = require('./server/routes/api');
 
 const app = express();
-const port = process.env.PORT || 3000;
+let port = process.env.PORT || 3000;
+//app.set('port', (process.env.PORT || 3000));
 
 // Logger
 app.use(logger('dev'));
@@ -83,4 +84,3 @@ app.use(function (req, res, next) {
 app.listen(port, () => {
 	console.log(`Server is running on ${port}`);
 });
-
